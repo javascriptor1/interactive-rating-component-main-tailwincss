3ca66d6b2eecb5ac1b555864b798c6b6b0171b75
Mohammed Fakih <mhd.fakih@gmail.com>
2023-07-02 22:52:17 +0300

    done

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-'use strict'
+"use strict";
+
+const ratingForm = document.querySelector('form[action="thanks.html"]');
+console.log(ratingForm);
+
+ratingForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let selection = 0;
+  const ratings = document.querySelectorAll("input");
+
+  for (let i = 0; i < ratings.length; i++) {
+    if (ratings[i].checked) {
+      selection++;
+      break;
+    }
+  }
+  if (selection > 0) {
+    ratingForm.submit();
+  } else {
+    alert("Please select a rating to proceed 🤚🛑");
+  }
+});
